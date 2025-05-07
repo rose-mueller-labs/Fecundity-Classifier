7338cb970bbd208af80e2446d533f2749e01aa09
sn82978 <shreyanakum2@gmail.com>
2025-05-07 16:53:28 -0700

    correct*** is whats on the slides

--- Plots/ScatterComp/scatter.py
+++ Plots/ScatterComp/scatter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("/home/drosophila-lab/Documents/Fecundity/CNN-Classifier/Plots/HumanBotComp/MoDataV1_5-4_tiles_MoDataV1.csv_sums_CSV.csv")
 
@@ -19,6 +20,16 @@ for key, value in sum_v_diff.items():
     sum_v_diff[key] = value/sum_v_diff_cnts[key]
 
 plt.scatter(list(sum_v_diff.keys()), list(sum_v_diff.values()))
+
+coeffs = np.polyfit(list(sum_v_diff.keys()), list(sum_v_diff.values()), 1)
+slope = coeffs[0]
+cept = coeffs[1]
+
+x_line = np.linspace(min(list(sum_v_diff.keys())), max(list(sum_v_diff.keys())), 100)
+y_line = slope * x_line + cept
+print("slope: ", slope)
+plt.plot(x_line, y_line, color="red")
 plt.xlabel("Correct (Average) Sum")
 plt.ylabel("Bot Absolute Difference from Sum")
-plt.savefig("Correct**")
+plt.title("Average Sum v. Bot Absolute Difference")
+plt.savefig("Correct***")
